76eac910056c455e455c13042ae6e4ac1e0e39e1
javel20 <javier_el_balla@hotmail.com>
2018-02-09 10:53:39 -0500

    routes

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,47 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/* 
+Route::get('/', function () {
+    return view('principal.ini');
+});
+ */
 
+Route::get('/principal/ini', function () {
+    return view('principal.ini');
+});
+
+
+Route::get('/principal/nosotros', function () {
+    return view('principal.nosotros');
+});
+
+
+// ----------------------------
+
+    Route::resource('accesos','AccesosController');
+
+    Route::resource('users','UsersController');
+
+    Route::resource('tipotrabajadors','RolesController');
+
+    Route::resource('proyectos','ProyectosController');
+    Route::get('/proyectosession','ProyectosController@indexporlogin');
+
+    Route::resource('comunicacions','ComunicacionsController');
+
+    Route::resource('tareas','TareasController');
+
+    Route::resource('fotos','FotosController');
+
+    Route::resource('docplanos','DocPlanosController');
+
+    //Route::post('userLogeo','Auth\LoginController@postLogin');
+
+
+
+// --------------------------
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
